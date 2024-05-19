4d022c43ce7fb0c2bbed3eba18367e2e065553b5
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2024-05-19 10:25:57 -0300

    better code, better results

--- index.js
+++ index.js
@@ -1,62 +1,85 @@
 const { Octokit } = require("@octokit/rest");
+const fs = require("fs").promises;
 
 const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN,
 });
 
-async function getRepoContent(owner, repo, path) {
-  const { data } = await octokit.repos.getContent({
-    owner,
-    repo,
-    path,
-  });
+let logFile = "./logs.txt";
 
-  let files = [];
+/**
+ * Get the content of a repository
+ * @param {string} owner The owner of the repository
+ * @param {string} repo The name of the repository
+ * @param {string} path The path to the directory
+ */
+async function getRepoContent(owner, repo, path) {
+  const { data } = await octokit.repos.getContent({ owner, repo, path });
+
+  const files = [];
+  const dirPromises = [];
+
   for (let item of data) {
     if (item.path.includes(".vitepress") || item.path.includes(".svg")) {
       continue;
     }
     if (item.type === "dir") {
-      files = files.concat(await getRepoContent(owner, repo, item.path));
+      dirPromises.push(getRepoContent(owner, repo, item.path));
     } else if (item.type === "file") {
       files.push({ name: item.name, path: item.path });
     }
   }
 
+  const dirFiles = await Promise.all(dirPromises);
+  for (const dirFile of dirFiles) {
+    files.push(...dirFile);
+  }
+
   return files;
 }
 
+/**
+ * Find missing docs
+ * @returns {Promise<void>}
+*/
 async function findMissingDocs() {
-  const frameworkFiles = await getRepoContent("yuiphp", "core", "src");
-  const docsFiles = await getRepoContent("yuiphp", "api-docs", "docs");
+  const [frameworkFiles, docsFiles] = await Promise.all([
+    getRepoContent("yuiphp", "core", "src"),
+    getRepoContent("yuiphp", "api-docs", "docs"),
+  ]);
+
+  const docsFilesSet = new Set(docsFiles.map((file) => file.name));
 
   const missingDocs = frameworkFiles.filter(
-    (file) =>
-      !docsFiles.some(
-        (docFile) => docFile.name === file.name.replace(".php", ".md")
-      )
+    (file) => !docsFilesSet.has(file.name.replace(".php", ".md"))
   );
 
+  let consoleOutput = missingDocs
+    .map((file) => {
+      const parts = file.path.split("/");
+      const coloredPath = parts
+        .map((part, index) => {
+          if (index === parts.length - 1) {
+            return `\x1b[33m${part}\x1b[0m`;
+          } else {
+            return `\x1b[36m${part}\x1b[0m`;
+          }
+        })
+        .join("/");
+      return coloredPath;
+    })
+    .join("\n");
+
+  let output = new Date().toLocaleString() + "\nMissing docs:\n" + missingDocs.map((file) => file.path).join("\n") + "\n";
+
   console.log("Missing docs:" + missingDocs.length + "\n");
-  console.log(
-    missingDocs
-      .map((file) => {
-        const parts = file.path.split("/");
-        const coloredPath = parts
-          .map((part, index) => {
-            if (index === parts.length - 1) {
-              // Colorir em amarelo
-              return `\x1b[33m${part}\x1b[0m`;
-            } else {
-              // Colorir em ciano
-              return `\x1b[36m${part}\x1b[0m`;
-            }
-          })
-          .join("/");
-        return coloredPath;
-      })
-      .join("\n")
-  );
+  console.log(consoleOutput);
+
+  await fs.writeFile(logFile, output);
 }
 
 findMissingDocs().catch(console.error);
+
+setInterval(function(){
+  findMissingDocs().catch(console.error);
+}, 14400000);
